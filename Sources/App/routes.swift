158ce5138270d3b3cb5110477aac47fd89369562
CURTIS DUNNE <curtisdunne@CURTISs-MacBook-Pro-2.local>
2018-09-05 09:59:37 -0600

    Add Google authentication to login from website

--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -8,19 +8,23 @@ import Fluent
 /// [Learn More →](https://docs.vapor.codes/3.0/getting-started/structure/#routesswift)
 public func routes(_ router: Router) throws {
   // Basic "Hello, world!" example
-  router.get("hello") { req in
+    router.get("hello") { req in
     return "Hello, world!"
-  }
+    }
 
-  let acronymsController = AcronymsController()
-  try router.register(collection: acronymsController)
+    let acronymsController = AcronymsController()
+    try router.register(collection: acronymsController)
 
-  let usersController = UsersController()
-  try router.register(collection: usersController)
+    let usersController = UsersController()
+    try router.register(collection: usersController)
 
-  let categoriesController = CategoriesController()
-  try router.register(collection: categoriesController)
+    let categoriesController = CategoriesController()
+    try router.register(collection: categoriesController)
 
-  let websiteController = WebsiteController()
-  try router.register(collection: websiteController)
+    let websiteController = WebsiteController()
+    try router.register(collection: websiteController)
+    
+    let imperialController = ImperialController()
+    try router.register(collection: imperialController)
+    
 }
